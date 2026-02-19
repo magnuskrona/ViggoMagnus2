7432d81a78f6ede1dcd30bf19107161eb81e541b
Viggo Krona <viggo.krona@gmail.com>
2026-02-19 15:47:00 +0100

    Added login and register.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
+using ViggoMagnus.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IUserService, UserService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseRouting();
 
-
-// test
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
